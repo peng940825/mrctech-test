1d1bd54b9749600640e555c6fffb73498137056c
Jun <peng940825@gmail.com>
2021-06-16 02:14:53 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script>
 import Header from '@/views/Header.vue';
 import Menu from '@/views/Menu.vue';
+import Footer from '@/views/Footer.vue';
 
 export default {
-  components: { Header, Menu },
+  components: { Header, Menu, Footer },
 };
 </script>
 
 <template>
   <Header />
   <Menu />
+  <Footer />
 </template>
 
 <style>
@@ -20,5 +22,6 @@ export default {
 body {
   margin: 0;
   height: 3000px;
+  background-color: cadetblue;
 }
 </style>
---
+++ src/views/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <div class="home">
+      <i class="bi bi-house-door"></i>
+      <span>Home</span>
+    </div>
+    <div class="trending">
+      <i class="bi bi-star"></i>
+      <span>Trending</span>
+    </div>
+    <div class="subscription">
+      <i class="bi bi-play-btn"></i>
+      <span>Subscription</span>
+    </div>
+    <div class="library">
+      <i class="bi bi-calendar-check"></i>
+      <span>Library</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~@/scss/views/footer.scss';
+</style>
